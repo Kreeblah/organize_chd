29caff367e1e13da5b98e5861fe1490760fd522b
Kreeblah <kreeblah@gmail.com>
2022-05-22 14:21:37 -0700

    Add license header

--- organize_chd.zsh
+++ organize_chd.zsh
@@ -1,5 +1,21 @@
 #!/usr/bin/env zsh
 
+# organize_chd
+# Copyright (C) 2022 Kreeblah
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 function do_compress_chd {
 	if ! type "chdman"; then
 		echo "chdman must be in your path to create CHD files"
